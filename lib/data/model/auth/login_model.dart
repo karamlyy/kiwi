345d7809595ac063f5c392db72ecadc16c8bbe2d
Karam K. Afandi <karam_afandi@yahoo.com>
2025-07-31 16:16:45 +0400

    login model update

--- lib/data/model/auth/login_model.dart
+++ lib/data/model/auth/login_model.dart
@@ -20,37 +20,18 @@ class User {
   int? id;
   String? username;
   String? name;
-  String? company;
-  String? branch;
-  int? branchId;
-  int? companyId;
-  int? employeeId;
-  int? userRoleId;
-  String? userRole;
 
   User({
     this.id,
     this.username,
     this.name,
-    this.company,
-    this.branch,
-    this.branchId,
-    this.companyId,
-    this.employeeId,
-    this.userRoleId,
-    this.userRole,
+
   });
 
   factory User.fromJson(Map<String, dynamic> json) => User(
         id: json["id"],
         username: json["username"],
         name: json["username"],
-        company: json["company"],
-        branch: json["branch"],
-        branchId: json["branchId"],
-        companyId: json["companyId"],
-        employeeId: json["employeeId"],
-        userRoleId: json["userRoleId"],
-        userRole: json["userRole"],
+
       );
 }
